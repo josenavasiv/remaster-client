e227a3e4687e70cdf1f56dcd01a53e5cef81689d
Alexander <josenavasiv@gmail.com>
2023-04-01 02:21:23 -0700

    Added Linking to notifications (However need to add uploaded notifications -> notifications sent from favorite user, and tagged to be a comment or description algorithm that creates notifications in the backend)

--- components/notifications/notification.tsx
+++ components/notifications/notification.tsx
@@ -11,15 +11,15 @@ type NotificationProps = {
 export const notificationFormat = (notification: NotificationFragment): JSX.Element => {
     switch (notification.notificationType) {
         case NotificationType.Liked:
-            if (notification.artwork)
+            if (notification.comment)
                 return (
                     <span>
-                        liked your artwork <span className="font-bold">{notification.artwork?.title}</span>
+                        liked your comment <span className="font-bold">{notification.comment?.comment} </span>
                     </span>
                 );
             return (
                 <span>
-                    liked your comment <span className="font-bold">{notification.comment?.comment}</span>
+                    liked your artwork <span className="font-bold">{notification.artwork?.title}</span>
                 </span>
             );
         case NotificationType.Commented:
@@ -43,20 +43,37 @@ export const notificationFormat = (notification: NotificationFragment): JSX.Elem
     }
 };
 
+const notificationLink = (notification: NotificationFragment): string => {
+    switch (notification.notificationType) {
+        case NotificationType.Liked:
+        case NotificationType.Commented:
+        case NotificationType.Replied:
+            return `/artwork/${notification.artwork?.id}`;
+        case NotificationType.Uploaded:
+            return '/';
+        case NotificationType.Tagged:
+            return '/';
+        default:
+            return '/';
+    }
+};
+
 export default function Notification({ notification }: NotificationProps) {
     return (
         <div className="w-full h-full">
-            <div className={`flex gap-2 h-16 rounded-lg ${notification.isRead ? 'bg-slate-300' : 'bg-pink-500'} `}>
-                <NotificationNotifier id={notification.notifier.id} avatarUrl={notification.notifier.avatarUrl} />
-                <div className="self-center w-full pr-3">
-                    <span className="font-bold">{notification.notifier.username} </span>
-                    {notificationFormat(notification)}
+            <Link href={notificationLink(notification)}>
+                <div className={`flex gap-2 h-16 rounded-lg ${notification.isRead ? 'bg-slate-300' : 'bg-pink-500'} `}>
+                    <NotificationNotifier id={notification.notifier.id} avatarUrl={notification.notifier.avatarUrl} />
+                    <div className="self-center w-full pr-3">
+                        <span className="font-bold">{notification.notifier.username} </span>
+                        {notificationFormat(notification)}
+                    </div>
                 </div>
-            </div>
-            <div className="flex justify-between px-0.5 pt-0.5 text-[10px] font-bold text-black/60">
-                <span className="">{getRelativeDate(notification.createdAt)}</span>
-                {!notification.isRead && <NotificationMarkAsRead notificationId={notification.id} />}
-            </div>
+                <div className="flex justify-between px-0.5 pt-0.5 text-[10px] font-bold text-black/60">
+                    <span className="">{getRelativeDate(notification.createdAt)}</span>
+                    {!notification.isRead && <NotificationMarkAsRead notificationId={notification.id} />}
+                </div>
+            </Link>
         </div>
     );
 }
